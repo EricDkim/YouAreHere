f2142475ad42a85191c8bcbc5a983b777e0f2252
John Rose <jrose@ggc.edu>
2017-02-27 17:21:21 -0500

    Added switch statement to handle multiple floor plans.

--- www/js/APIKeys.js
+++ www/js/APIKeys.js
@@ -15,10 +15,39 @@ var IA_API_SECRET = '/WR8nloUygtkltkPzRW3i/Lu5o6YR21BExwqYdq8GDlH2rLelYbe4NwxIWU
 
 // Floorplan ID https://developer.indooratlas.com/venues
 // have a selector (if statement or switch statement) to get the value of the building then use that value
-// and use the appropriate floorplan_id for the app.
+// and use the appropriate floorplan_id for the app
+
+// Create variable to grab the current building selector value
+
+var building_selection = $(this).val(); //<-- Setting this value globally in index.js might fix issue
+
+// Create a switch statement to handle the building floor plan id
+switch(building_selection) {
+    case 'A':
+        alert("Building A floor plan id is being used!");
+        // floorplan id for A
+        var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
+        break;
+    case 'B':
+        alert("Building B floor plan id is being used!");
+        // Floor plan has not been created
+        break;
+    case 'C':
+        alert("Building C floor plan id is being used!");
+        // Floor plan has not been created
+        break;
+    case 'H':
+        alert("Building H floor plan id is being used!");
+        // floorplan id for H
+        var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9';
+       // break;
+    //default : 
+      //  alert("You did not select a building.");
+}
+
 
 // floorplan id for A
- var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
+//var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
  
 // floorplan id for H
  // var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9'
