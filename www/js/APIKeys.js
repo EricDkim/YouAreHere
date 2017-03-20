14d4314b23ba63ad112c54ab51356c8bbd3da847
EricDkim <dkim3@ggc.edu>
2017-03-20 03:41:55 -0400

    Keys script were edited

--- www/js/APIKeys.js
+++ www/js/APIKeys.js
@@ -7,7 +7,7 @@
 // API KEYS FOR INDOORATLAS SERVICES AND GOOGLE MAPS JAVASCRIPT API
 
 // Get Google Maps API Key from here: https://console.developers.google.com/apis/credentials
-var GOOGLE_API_KEY = 'AIzaSyCLTAgyoDNgxcQtf9EUJ0NxX7SMrE9lYr0';
+var GOOGLE_API_KEY = 'AIzaSyAzXNyHtYRnUl8l_ZyIWJ_NsTf-AYwBhBA';
 
 // Get IndoorAtlas API Key and Secret from here https://app.indooratlas.com/apps
 var IA_API_KEY = 'acb20002-c1c8-4da7-882a-4ec0fbffad82';
@@ -19,35 +19,36 @@ var IA_API_SECRET = '/WR8nloUygtkltkPzRW3i/Lu5o6YR21BExwqYdq8GDlH2rLelYbe4NwxIWU
 
 // Create variable to grab the current building selector value
 
-var building_selection = $(this).val(); //<-- Setting this value globally in index.js might fix issue
+//var building_selection = $(this).val(); //<-- Setting this value globally in index.js might fix issue
+
+// var building_selection = document.getElementById('select-building');
 
 // Create a switch statement to handle the building floor plan id
-switch(building_selection) {
-    case 'A':
-        alert("Building A floor plan id is being used!");
-        // floorplan id for A
-        var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
-        break;
-    case 'B':
-        alert("Building B floor plan id is being used!");
-        // Floor plan has not been created
-        break;
-    case 'C':
-        alert("Building C floor plan id is being used!");
-        // Floor plan has not been created
-        break;
-    case 'H':
-        alert("Building H floor plan id is being used!");
-        // floorplan id for H
-        var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9';
-       // break;
-    //default : 
-      //  alert("You did not select a building.");
-}
-
+// switch(sbuilding_selection) {
+//     case 'A':
+//         alert("Building A floor plan id is being used!");
+//         // floorplan id for A
+//         var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
+//         break;
+//     case 'B':
+//         alert("Building B floor plan id is being used!");
+//         // Floor plan has not been created
+//         break;
+//     case 'C':
+//         alert("Building C floor plan id is being used!");
+//         // Floor plan has not been created
+//         break;
+//     case 'H':
+//         alert("Building H floor plan id is being used!");
+//         // floorplan id for H
+//         var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9';
+//        // break;
+//     //default : 
+//       //  alert("You did not select a building.");
+// }
 
 // floorplan id for A
-//var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
+var IA_FLOORPLAN_ID = 'c49491e5-6c4b-43a4-9c83-e4853435a95b';
  
 // floorplan id for H
- // var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9'
+var IA_FLOORPLAN_ID = '7de47cbe-c232-4875-b3d7-324b1ba15ed9'
